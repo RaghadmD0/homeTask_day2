57763b29653d099555aa36582c9f7143a543296a
RaghadmD0 <raghadmdosari@gmail.com>
2025-07-14 19:52:58 +0300

    Added full EDA code and dataset

--- Eda.py
+++ Eda.py
@@ -1,1 +1,20 @@
-import pandas as pd 
+import numpy as np 
+import pandas as pd
+
+def main():
+    dataset = pd.read_csv("studentsData.csv")
+    
+    print("Columns:", ", ".join(dataset.columns))
+    print("\nFull data:\n", dataset)
+    
+    print("\nData summary:")
+    print(dataset.describe())
+    
+    print("\nNumber of students per class:")
+    print(dataset['class'].value_counts())
+    
+    print("\nAverage marks per class:")
+    print(dataset.groupby('class')['mark'].mean())
+
+if __name__ == "__main__":
+    main()
